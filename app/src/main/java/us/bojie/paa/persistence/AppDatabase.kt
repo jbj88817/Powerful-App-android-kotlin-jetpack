fb289edfdb60e858cb0776ff04285313f9436c8c
Bojie Jiang <jiangbojie@gmail.com>
2019-11-30 11:55:18 -0800

    Room Persistence Setup

--- app/src/main/java/us/bojie/paa/persistence/AppDatabase.kt
+++ app/src/main/java/us/bojie/paa/persistence/AppDatabase.kt
@@ -1,5 +1,16 @@
 package us.bojie.paa.persistence
 
-abstract class AppDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import us.bojie.paa.model.AccountProperties
+import us.bojie.paa.model.AuthToken
 
+@Database(entities = [AuthToken::class, AccountProperties::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun getAuthTokenDao(): AuthTokenDao
+    abstract fun getAccountPropertiesDao(): AccountPropertiesDao
+    companion object {
+        const val DATABASE_NAME = "app_db"
+    }
 }
--- app/src/main/java/us/bojie/paa/persistence/AuthTokenDao.kt
+++ app/src/main/java/us/bojie/paa/persistence/AuthTokenDao.kt
@@ -1,5 +1,16 @@
 package us.bojie.paa.persistence
 
-interface AuthTokenDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import us.bojie.paa.model.AuthToken
 
+@Dao
+interface AuthTokenDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(authToken: AuthToken): Long
+
+    @Query("UPDATE auth_token SET token = null WHERE account_pk = :pk")
+    fun nullifyToken(pk: Int) : Int
 }
